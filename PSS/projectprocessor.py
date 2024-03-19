eeba36981adbe668e6e9651ddf80182f3ad9c8a4
Victor Hucklenbroich <Victor.Hucklenbroich@kpit.com>
2024-03-19 18:01:04 +0100

    Add compilation via Makefile, logging and minor fixed to program flow

--- PSS/projectprocessor.py
+++ PSS/projectprocessor.py
@@ -1,11 +1,16 @@
 import locale
+import mimetypes
 import os
 import subprocess
 from pathlib import Path
 
+from pss import DATA_PATH
+from pss import PSS_PREFIX
+
 
 def get_binaries(p0, p1):
-    return compile_program(search_paths(p0), 0), compile_program(search_paths(p1), 1)
+    clean()
+    return compile_program(p0, 0), compile_program(p1, 1)
 
 
 def search_paths(directory):
@@ -13,32 +18,89 @@ def search_paths(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith('.c') | file.endswith('.cpp'):
-                paths.append(os.path.join(root, file))
+                path = os.path.join(root, file)
+                print(path)
+                paths.append(path)
+
     return paths
 
 
-def compile_program(files, n: int) -> [str]:
+def compile_program(dir, n: int) -> [str]:
+    print(PSS_PREFIX + "compiling p" + str(n))
+    if has_makefile(dir):
+        return compile_program_cmake(dir, n)
+    else:
+        return compile_program_gcc(dir, n)
+
+
+def compile_program_cmake(dir, n: int) -> [str]:
+    binaries = []
+    exclusions = ['.DS_Store', 'Makefile', 'README', '.1', '.hpp', 'c']
+    make = check_dependencies()[1]
+    make_cmd = [make, 'all']
+    subprocess.run(make_cmd, cwd=dir)
+    dir = os.path.join(dir, 'src')
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            mime = mimetypes.guess_type(file)
+            if mime[0] is None:
+                file = os.path.join(dir, file)
+                include: bool = True
+                for exclusion in exclusions:
+                    if file.endswith(exclusion):
+                        include = False
+                        break
+
+                if include:
+                    binaries.append(file)
+
+    print(PSS_PREFIX + "compiled p" + str(n) + " successfully using Makefile")
+    return binaries
+
+
+def compile_program_gcc(dir, n: int) -> [str]:
+    files = search_paths(dir)
     binaries = []
     gcc = check_dependencies()[0]
-    output_dir = os.path.join("data", "binaries") + str(n)
+    output_dir = os.path.join(DATA_PATH, "binaries") + str(n)
     for file in files:
         gcc_cmd = [gcc, file, '-o']
         binary = os.path.join(output_dir, Path(file).stem)
         gcc_cmd += [binary]
         mkdir_cmd = ['mkdir', '-p', output_dir]
+        print(mkdir_cmd)
         subprocess.run(mkdir_cmd)
+        print(gcc_cmd)
         subprocess.run(gcc_cmd)
         binaries += [binary]
 
+    print(PSS_PREFIX + "compiled p" + str(n) + " successfully using gcc")
     return binaries
 
 
+def clean():
+    path = DATA_PATH
+    for dirs in os.walk(path):
+        for dir in dirs[1]:
+            dir = os.path.join(path, dir)
+            if has_makefile(dir):
+                make_clean(dir)
+    clear_temporary_dirs()
+    print(PSS_PREFIX + "clean successful")
+
+
+def make_clean(dir):
+    make = check_dependencies()[1]
+    make_cmd = [make, 'clean']
+    subprocess.check_output(make_cmd, cwd=dir)
+
+
 def clear_temporary_dirs():
-    path = os.path.join("data", "binaries")
+    path = os.path.join(DATA_PATH, "binaries")
     for i in range(2):
         cmd = ['rm', '-rf', (path + str(i))]
         try:
-            subprocess.check_call(cmd)
+            subprocess.check_output(cmd)
         except subprocess.CalledProcessError:
             continue
 
@@ -48,8 +110,13 @@ def decode(bytes) -> str:
     return bytes.decode(encoding)
 
 
+def has_makefile(dir) -> bool:
+    makefile = os.path.join(dir, "Makefile")
+    return os.path.isfile(makefile)
+
+
 def check_dependencies():
-    required = ['gcc']
+    required = ['gcc', 'make']
 
     dependencies = []
     for dependency in required:
--- PSS/pss.py
+++ PSS/pss.py
@@ -2,8 +2,11 @@ import os
 
 import numpy as np
 
-import projectprocessor
 import preprocessor as preproc
+import projectprocessor
+
+DATA_PATH = os.path.join(os.getcwd(), 'data')
+PSS_PREFIX = "[PSS] "
 
 
 def compute_similarity(feat0, feat1) -> float:
@@ -14,29 +17,33 @@ def compute_similarity(feat0, feat1) -> float:
     return np.sqrt(2) - np.sqrt(sum)
 
 
-def sim_cg(p0: str, p1: str) -> float:
-    (b0, b1) = projectprocessor.get_binaries(p0, p1)
+def sim_cg(b0: str, b1: str) -> float:
+    print(PSS_PREFIX + "calculating feature vector v0")
     v0: list = np.ndarray.tolist(preproc.compute_v(b0))[0]
+    print(PSS_PREFIX + "v0:" + str(v0))
+    print(PSS_PREFIX + "calculating feature vector v1")
     v1: list = np.ndarray.tolist(preproc.compute_v(b1))[0]
+    print(PSS_PREFIX + "v1:" + str(v1))
     return compute_similarity(v0, v1)
 
 
-def sim_cfg(p0: str, p1: str) -> float:
-    (b0, b1) = projectprocessor.get_binaries(p0, p1)
+def sim_cfg(b0: str, b1: str) -> float:
+    print(PSS_PREFIX + "calculating feature vector w0")
     w0: list = np.ndarray.tolist(preproc.compute_w(b0))
+    print(PSS_PREFIX + "w0:" + str(w0))
+    print(PSS_PREFIX + "calculating feature vector w1")
     w1: list = np.ndarray.tolist(preproc.compute_w(b1))
+    print(PSS_PREFIX + "w1:" + str(w1))
     return compute_similarity(w0, w1)
 
 
 def compare(p0: str, p1: str) -> float:
-    pss_value = (sim_cg(p0, p1) + sim_cfg(p0, p1)) / (2 * np.sqrt(2))
-    projectprocessor.clear_temporary_dirs()
+    (b0, b1) = projectprocessor.get_binaries(p0, p1)
+    pss_value = (sim_cg(b0, b1) + sim_cfg(b0, b1)) / (2 * np.sqrt(2))
     return pss_value
 
 
 if __name__ == '__main__':
-    path = os.getcwd()
-    path += "/data/"
-    p0 = path + "p0"
-    p1 = path + "p1"
-    print("PSS(p0, p1) = " + str(compare(p0, p1)))
+    p0 = os.path.join(DATA_PATH, "p0")
+    p1 = os.path.join(DATA_PATH, "p1")
+    print(PSS_PREFIX + "pss(p0, p1) = " + str(compare(p0, p1)))
