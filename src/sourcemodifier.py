e879d81ff5c58e13db57ba5f15182c8e65cb1599
Victor Hucklenbroich <Victor.Hucklenbroich@kpit.com>
2024-03-29 16:19:00 +0100

    Fix lines of code metric

--- src/sourcemodifier.py
+++ src/sourcemodifier.py
@@ -1,9 +1,11 @@
+import json
 import random
 import string
+import subprocess
 from enum import Enum
 
 import preprocessor as preproc
-from src import logger
+from src import logger, constants
 
 
 class ModMode(Enum):
@@ -29,11 +31,9 @@ def obfuscate(p: str, noise_per_loc: float = 0.1):
     sources: [str] = preproc.search_paths(p)
     noise_added: int = 0
     for source in sources:
-        with open(source, 'r') as s:
-            for count, line in enumerate(s):
-                pass
         generated_functions = []
-        for i in range(int(count * noise_per_loc)):
+        loc: int = calculate_loc(source)
+        for i in range(int(loc * noise_per_loc)):
             generated_functions += [generate_function(generated_functions)]
 
         with open(source, "a") as s:
@@ -66,3 +66,11 @@ def generate_function(available_functions: [dict]) -> dict:
 
     f_def += '} \n'
     return dict(name=f_name, return_type=f_return, function=f_def)
+
+
+def calculate_loc(source: str) -> int:
+    scc_cmd = [constants.scc()]
+    scc_cmd += [source, '-f', 'json']
+
+    data = json.loads(constants.decode(subprocess.check_output(scc_cmd)))
+    return data[0]['Code']
